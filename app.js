711b704eb1f66e848bc48b37e97bdbc0b6c1a3cd
austinogiza <44683018+austinogiza@users.noreply.github.com>
2020-05-13 22:52:37 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// const title = document.getElementById('task-title');
+
+// title.innerText = 'Hello World';
+
+// title.textContent = 'Today was a great day';
+
+const title = document.querySelector('#task-title');
+
+console.log(title);
+
+const items = document.getElementsByClassName('collection-item');
+
+console.log(items);
+
+console.log(items[0]);
+
+items[0].style.color = 'blue';
